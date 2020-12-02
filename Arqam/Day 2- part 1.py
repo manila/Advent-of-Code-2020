00a7acb24ef5bcdf82e9fedc713da3c16f53f57b
ArqamAdiyan <75317921+ArqamAdiyan@users.noreply.github.com>
2020-12-02 16:46:32 +0400

    Day 2 Submitted

---
+++ Arqam/Day 2- part 1.py
@@ -0,0 +1,16 @@
+x=0
+f=open('file2.txt','r')
+for s in f:
+    s=s.split(': ')
+    a=str(s[0][-1])
+    s[0]=s[0][:-2]
+    b=s[0].split('-')
+    s.pop(0)
+    cnt= s[0].count(a)
+    if cnt>=int(b[0]) and cnt<=int(b[1]):
+        x+=1
+print('count-->',x)
+
+
+
+
