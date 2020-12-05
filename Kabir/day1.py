e8835f9edb1040cc16da1c945f7b7d485960b0f2
KabirSamsi <samsikabir@gmail.com>
2020-12-05 10:53:42 -0800

    Completed day 1 challenge

---
+++ Kabir/day1.py
@@ -0,0 +1,22 @@
+file = open("day1entries.txt", "r")
+entries = []
+num1 = 0
+num2 = 0
+product = 0
+
+for line in file:
+    entries.append(int(line))
+
+for x in range(len(entries)):
+    for y in range(len(entries)):
+
+        #Evaluate sum and make sure entry indices are not the same
+        if entries[x]+entries[y] == 2020 and x != y:
+            num1 = entries[x]
+            num2 = entries[y]
+            product = entries[x]*entries[y]
+            break
+
+print("Number 1: {}".format(num1))
+print("Number 2: {}".format(num2))
+print("Product: {}".format(product))
---
+++ Kabir/day1part2.py
@@ -0,0 +1,26 @@
+file = open("day1entries.txt", "r")
+entries = []
+num1 = 0
+num2 = 0
+num3 = 0
+product = 0
+
+for line in file:
+    entries.append(int(line))
+
+for x in range(len(entries)):
+    for y in range(len(entries)):
+        for z in range(len(entries)):
+
+            #Evaluate sum and make sure entry indices are not the same
+            if entries[x]+entries[y]+entries[z] == 2020 and x != y and x !=z  and y != z:
+                num1 = entries[x]
+                num2 = entries[y]
+                num3 = entries[z]
+                product = entries[x]*entries[y]*entries[z]
+                break
+
+print("Number 1: {}".format(num1))
+print("Number 2: {}".format(num2))
+print("Number 3: {}".format(num3))
+print("Product: {}".format(product))
